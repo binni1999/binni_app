1f0f94cacfecaadb119c69560b43306c8e5c93b8
pankaj binwal <116300718+binni1999@users.noreply.github.com>
2023-07-04 22:29:41 +0530

    Add files via upload

---
+++ SpringBootApp/src/test/java/net/binni/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.binni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
